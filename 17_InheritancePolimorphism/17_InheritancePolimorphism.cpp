be4866ad4473779bc91786a8ba402450fb8c727e
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-11-10 11:26:16 +0200

    Polimorphism, virtual, override

--- 17_InheritancePolimorphism/17_InheritancePolimorphism.cpp
+++ 17_InheritancePolimorphism/17_InheritancePolimorphism.cpp
@@ -1,139 +1,146 @@
-#include<iostream>
-#include<Windows.h>
-
-
-
+﻿#include<iostream>
+#include <windows.h> 
 using namespace std;
 
-
-void Gotoxy(int x, int y) {
-	COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-class Point {
-private:
-	int m_x;
-	int m_y;
-public:
-	Point() :m_x(0), m_y(0) {}
-	Point(int x, int y) :m_x(x), m_y(y) {}
-	int operator ++ (int n) {
-		return m_x++, m_y++;
-	}
-	int operator -- (int n) {
-		return m_x--, m_y--;
-	}
-	int operator ! () {
-		m_x = (-m_x);
-		m_y = (-m_y);
-		return m_x, m_y;
-	}
-	bool operator < (Point p) {
-		return (this->m_x < p.m_x&& this->m_y < p.m_y);
-	}
-	bool operator > (Point p) {
-		return (this->m_x > p.m_x && this->m_y > p.m_y);
-	}
-	void Print() {
-		cout << "X:" << m_x << endl;
-		cout << "Y:" << m_y << endl;
-	}
-	int GetX() {
-		return m_x;
-	}
-	int GetY() {
-		return m_y;
-	}
-};
-class Vector
+class Car
 {
-private:
-	Point* points;
-	int countPoint;
+protected:
+	string mark;//brand
+	string model;
+	int year;
+	float volume;
+	string typeFuel;
+	int maxSpeed;
+	int hoursePower;
 public:
-	Vector() {
-		points = nullptr;
-		countPoint = 0;
+	Car() :mark(""), model(""), year(0), volume(0), typeFuel(""), maxSpeed(0), hoursePower(0) {}
+	Car(string mark, string model,int year,float volume,string type,
+		int maxSpeed, int hoursePower) :mark(mark), model(model), year(year), volume(volume),
+		typeFuel(type), maxSpeed(maxSpeed), hoursePower(hoursePower) {}
+	virtual void Drive()const
+	{
+		cout << " I am a Car! I can drive with speed " << maxSpeed << "km/h" << endl;
 	}
-	Vector(int size) {
-		countPoint = size;
-		points = new Point[countPoint];
-		for (int i = 0; i < countPoint; i++) {
-			points[i] = Point(i, i);
-		}
+	virtual void Print()const
+	{
+		cout << "Mark of car : " << mark << endl;
+		cout << "Model of car : " << model << endl;
+		cout << "Year : " << year << endl;
+		cout << "Volume : " << volume << endl;
+		cout << "Type of fuel : " << typeFuel << endl;
+		cout << "Max Speed : " << maxSpeed << endl;
+		cout << "Hourse Power : " << hoursePower << endl;		
+	}
+	void SetSpeed(int speed)
+	{
+		if (speed >= 0)
+			maxSpeed = speed;
+	}
 
-	}
-	~Vector() {
-		delete[]points;
-	}
-	void Add(int x, int y) {
-		countPoint++;
-		Point* pt = new Point[countPoint];
-		if (countPoint > 1) {
-			for (int i = 0; i < countPoint; i++) {
-				pt[i] = points[i];
-			}
-		}
-		pt[countPoint - 1] = Point(x, y);
-		delete[]points;
-		points = pt;
-
-
-	}
-	Point& Pop(Point* points) {
-		countPoint--;
-		Point* pt = new Point[countPoint];
-		if (countPoint >= 1) {
-			for (int i = 0; i < countPoint; i++) {
-				pt[i] = points[i];
-			}
-		}
-		points = pt;
-		delete[]pt;
-	}
-	void Print() {
-		for (int i = 0; i < countPoint; i++) {
-			Gotoxy(points[i].GetX(), points[i].GetY());
-			cout << char(223);
-		}
-
-	}
 };
 
+class PoliceCar : public Car
+{
+	int soudVolume;
+public:
+	PoliceCar() : soudVolume(0), Car() {}
+	PoliceCar(string mark, string model, int year, float volume, string type,
+		int maxSpeed, int hoursePower, int sound) : soudVolume(sound), Car(mark,model,
+			year,volume,type, maxSpeed,hoursePower) {}
 
-void main() {
-	//===========part 1=============
-	Point p1 = Point(1,1);
-	p1.Print();
-	p1--;
-	p1.Print();
-	p1++;
-	p1.Print();
-	!p1;
-	p1.Print();
-	Point p2 = Point();
-	if (p1 > p2) {
-		cout << "p1 is biger" << endl;
+	void Drive()const override
+	{
+		cout << " I am a Police Car! I can drive with speed " << maxSpeed << "km/h" << endl;
 	}
-	else if (p1 < p2) {
-		cout << "p1 is smaller" << endl;
+	void Print()const override
+	{
+		Car::Print();
+		cout << "Sound volume " << soudVolume << endl;
 	}
-	//=================part 2=====================
-	/*Vector v = Vector();
-	int j = 20;
-	int k = 0;
-	for (int i = 0; i < 10; i++) {
-		v.Add(j, i);
-		j -= 2;
-	}
-	for (int i = 0; i < 10; i++) {
-		v.Add(k, i);
-		k += 2;
+	void BeepBeep()
+	{
+		Beep(1568, 400);
+		Beep(1245, 400);
+		Beep(1568, 400);
+		Beep(1245, 400);
+		Beep(1568, 400);
+		Beep(1245, 400);
+		Beep(1568, 400);
+		Beep(1245, 400);
 	}
 
-	v.Print();*/
+};
+class SportCar : public Car
+{
+	int turbo;
+public:
+	SportCar() : turbo(0), Car() {}
+	SportCar(string mark, string model, int year, float volume, string type,
+		int maxSpeed, int hoursePower, int turbo) : turbo(turbo), Car(mark, model,
+			year, volume, type, maxSpeed, hoursePower) {}
+
+	void Drive()const override
+	{
+		cout << " I am a Sport Car! I can drive with speed " << maxSpeed << "km/h" << endl;
+	}
+	void Print()const override
+	{
+		Car::Print();
+		cout << "Turbo  " << turbo << endl;
+	}
+	void Nitro()
+	{
+		maxSpeed += turbo;
+		cout << "Enabme turbo speed : " << maxSpeed<<"km/h";
+	}
+
+};
+void TestDrive(Car& car)
+{
+	car.SetSpeed(60);
+	car.Drive();
+
+}
+void main() 
+{
+	Car simpleCar("Daewoo", "Lanos", 2010, 1.5, "petrol", 100, 75);
+	simpleCar.SetSpeed(70);
+	simpleCar.Drive();
+	simpleCar.Print();
+	cout << endl << endl;
+
+	PoliceCar police("Toyota","Prius",2015,1.3,"electric+petrol",200,90,500);
+	police.SetSpeed(120);
+	police.Drive();
+	police.Print();
+	//police.BeepBeep();
+
+	SportCar sport("Porcshe", "911", 2022, 2.5, "petrol", 320, 500, 40);
+	sport.Drive();
+
+	TestDrive(simpleCar);
+	TestDrive(police);
+	TestDrive(sport);
+
+	Car* carSalon[3]
+	{
+		new Car("Daewoo", "Lanos", 2010, 1.5, "petrol", 100, 75),
+		new PoliceCar("Toyota","Prius",2015,1.3,"electric+petrol",200,90,500),
+		new SportCar("Porcshe", "911", 2022, 2.5, "petrol", 320, 500, 40)
+	};
+
+	for (int i = 0; i < 3; i++)
+	{
+		carSalon[i]->Print();
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		delete carSalon[i];
+	}
+
+	//p.Drive();
+
 
 
 }
